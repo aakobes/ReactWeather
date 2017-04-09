12a481c89f1d48e28a7de4cd0d83cc5191be8282
Aaryn Kobayashi <Aaryn@Aaryns-MacBook-Air.local>
2017-04-09 13:08:28 -0700

    Added div to about page

--- app/Components/About.jsx
+++ app/Components/About.jsx
@@ -2,8 +2,11 @@ var React = require('react');
 
 var About = (props) => {
   return (
+  	<div>
     <h3>About</h3>
+
     <p>Welcome to the about page!</p>
+    </div>
   )
 }
 module.exports = About;
